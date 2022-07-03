4d7faa6fe9426454066e252c1a750ae5e201c6fa
chirstiankaikai <ckaikai19@gmail.com>
2022-07-03 12:32:56 -0400

    added chatroom hbs

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,19 @@
 const router = require('express').Router();
-// const { Project, Discussion, User } = require('../models');
-// const withAuth = require('../utils/auth');
+const { Project, Discussion, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/landing', (req, res) => {
+    res.render('landing');
+});
+
 
 router.get('/', (req, res) => {
     res.render('dashboard');
+});
+
+
+router.get('/chatroom', (req, res) => {
+    res.render('chatroom');
 });
 
 
