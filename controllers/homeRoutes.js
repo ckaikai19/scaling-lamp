a8bd58e9d2cb253a4d560dbf7c3ce2e60af9cf5c
Yuchen Cui <yuchencui@hotmail.com>
2022-07-03 22:24:25 -0400

    Formate handle bars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,18 +2,18 @@ const router = require('express').Router();
 const { Project, Discussion, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/landing', (req, res) => {
-    res.render('landing');
+router.get('/', (req, res) => {
+    res.render('landing', { title : 'Landing', cssname: 'home.css' });
 });
 
 
-router.get('/', (req, res) => {
-    res.render('dashboard');
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard', { title : 'Dashboard', cssname: 'dashboard.css' });
 });
 
 
 router.get('/chatroom', (req, res) => {
-    res.render('chatroom');
+    res.render('chatroom' , { title : 'Chatroom', cssname: 'chatroom.css' });
 });
 
 
